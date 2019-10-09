0503318f06d6934103098661bcb0d3051efc6fe7
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-10-09 18:54:32 -0500

    palindrome, toString, split, reverse, join

--- utils.js
+++ utils.js
@@ -1,23 +1,11 @@
 
-var reverse = function(x) {
-    
+var isPalindrome = function(x) {
     var nums = x.toString().split('');
-    var sign;
-    var last;
-    var finalNumber = 0;
-    if (nums[0] == "-" || nums[0] == "+"){
-        sign = nums.shift();
-    }    
     nums.reverse();
-    if (parseInt(nums[0]) == 0){
-        nums.shift();
-    }
-    finalNumber= parseInt((sign == "-" ? sign : "")+(nums.join('').toString()));
-    //return finalNumber>=Number.MIN_SAFE_INTEGER && finalNumber<=Number.MAX_SAFE_INTEGER ? finalNumber : 0;
-    return finalNumber>=-1*Math.pow(2,31) && finalNumber<=(Math.pow(2,31)-1) ? finalNumber : 0;
+    return nums.join('') == x;
 };
 
-console.log(reverse(123));
-console.log(reverse(-123));
-console.log(reverse(120));
-console.log(reverse(1534236469));
+console.log(isPalindrome(121));
+console.log(isPalindrome(-121));
+console.log(isPalindrome(120));
+console.log(isPalindrome(10));
