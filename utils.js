c4286fae0a5056e3434cb55c690cb5c9db502b3f
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-10-09 17:59:12 -0500

    two sum

--- utils.js
+++ utils.js
@@ -1,22 +1,51 @@
-function resolveAfter2Seconds(x){
-    return new Promise(resolve => {
-        setTimeout(() => {
-          resolve(x)
-        },2000);
-    });
-}
 
-async function add1(x) {
-    const a =  await resolveAfter2Seconds(20);
-    const b =  await resolveAfter2Seconds(30);
-    return x+a+b;
-}
+var twoSum = function(nums, target) {
+    //2, 7, 11, 15
+    //found
+    
+        //--1
+        // 2
+        // 7,11,15
+        //found
+    
+        //--2
+        //7
+        //11,15
+        //found
+    
+        //--3
+        //11
+        //15
+        //found
+    
+        //--4    
+        //15
+        //
+        //found
+    
+    //return found
+    let first;
+    let targetIndex = -1;
+    let i=0;
+    
+    while (nums.length>0)
+        {
+            first = nums.shift();            
+            var targetNumbs = nums.forEach(function (value, index, array){
+                console.log(value);
+                if (parseInt(value) + parseInt(first) == parseInt(target)){
+                    targetIndex = index+1;                    
+                }                
+            });
+            
+            if (targetIndex != -1){
+                break;
+            }
+            i++;            
+        }
+    
+    return [i,targetIndex]
+        
+};
 
-async function add2(x){
-    const p_a = resolveAfter2Seconds(20);
-    const p_b = resolveAfter2Seconds(30);
-
-    return x + await p_a + await p_b;
-}
-
-export {add1,add2};
+twoSum([2,7,11,15],9);
