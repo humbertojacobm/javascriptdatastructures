decf7dcfc25afc8e3baa8e9616f88c70488275d7
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-10-09 19:05:14 -0500

    palindrome just number way, %, Math.Floor(x/y) , *10

--- utils.js
+++ utils.js
@@ -1,8 +1,18 @@
 
 var isPalindrome = function(x) {
-    var nums = x.toString().split('');
-    nums.reverse();
-    return nums.join('') == x;
+    
+  if(x<0 || (x % 10 == 0 && x != 0)){
+      return false;
+  }
+
+  let revertedNumber = 0;
+  let buffer = x;
+  while(x > revertedNumber){
+      revertedNumber = revertedNumber*10 + buffer%10;
+      buffer = Math.floor(buffer / 10);
+  }
+
+  return x == revertedNumber;
 };
 
 console.log(isPalindrome(121));
